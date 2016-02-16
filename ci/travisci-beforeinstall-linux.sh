ba9c698b7cb0fcc7f6f1f76be33ad511f1378af2
Joël R. Langlois <joel.r.langlois@gmail.com>
2016-02-16 06:28:43 -0500

    Added beginning of build scripts. Added iOS project to the UnitTestRunner.

---
+++ ci/travisci-beforeinstall-linux.sh
@@ -0,0 +1,17 @@
+sudo apt-get update --fix-missing
+sudo apt-get install -qq
+    clang
+    libfreetype6-dev
+    libx11-dev
+    libxinerama-dev
+    libxrandr-dev
+    libxcursor-dev
+    libxcomposite-dev
+    mesa-common-dev
+    libasound2-dev
+    freeglut3-dev
+    libcurl4-gnutls-dev+
+    libasound2-dev
+    libjack-dev
+sudo apt-get update --fix-missing
+sudo apt-get update -qq
