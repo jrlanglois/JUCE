90a9b158b1a5651a002c70782fc84ab4b1bb26e3
Joël R. Langlois <joel.r.langlois@gmail.com>
2021-02-02 15:44:12 -0500

    Added extended bit range reading to AIFF and WAV.
    This commit also includes:
    - µ-law support
    - a-law support
    - fixes all of the compatible extensions for AIFF and WAV
    - adds FLAC as a subformat to WAV
    - added (and in some places removed) sample rates to the custom readers/writers, so as to be to spec

--- modules/juce_audio_formats/codecs/juce_WindowsMediaAudioFormat.cpp
+++ modules/juce_audio_formats/codecs/juce_WindowsMediaAudioFormat.cpp
@@ -124,10 +124,10 @@ private:
 
 //==============================================================================
 static const char* wmFormatName = "Windows Media";
-static const char* const extensions[] = { ".mp3", ".wmv", ".asf", ".wm", ".wma", 0 };
+static const char* const extensions[] = { ".mp3", ".wmv", ".asf", ".wm", ".wma", ".wav", ".wave", ".w64", ".bwf", 0 };
 
 //==============================================================================
-class WMAudioReader   : public AudioFormatReader
+class WMAudioReader final : public AudioFormatReader
 {
 public:
     WMAudioReader (InputStream* const input_)
@@ -329,7 +329,7 @@ bool WindowsMediaAudioFormat::isCompressed()    { return true; }
 //==============================================================================
 AudioFormatReader* WindowsMediaAudioFormat::createReaderFor (InputStream* sourceStream, bool deleteStreamIfOpeningFails)
 {
-    std::unique_ptr<WindowsMediaCodec::WMAudioReader> r (new WindowsMediaCodec::WMAudioReader (sourceStream));
+    auto r = std::make_unique<WindowsMediaCodec::WMAudioReader> (sourceStream);
 
     if (r->sampleRate > 0)
         return r.release();
